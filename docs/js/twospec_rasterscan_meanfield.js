90c8870cc76b3f669b779fb71dedb2c3280ebefe
Riz <87264061+rizfn@users.noreply.github.com>
2023-10-10 14:40:22 +0200

    Add two species model

---
+++ docs/js/twospec_rasterscan_meanfield.js
@@ -0,0 +1,319 @@
+let { default: data_meanfield } = await import("../data/two_species/mean_field_data_r=1.json", { assert: { type: "json" } });
+let { default: data_stochastic } = await import("../data/two_species/stochastic_dynamics_r=1.json", { assert: { type: "json" } });
+// let { default: data_3D } = await import("../data/two_species/3D_stochastic_dynamics_r=1.json", { assert: { type: "json" } });
+let { default: data_wellmixed } = await import("../data/two_species/wellmixed_stochastic_dynamics_r=1.json", { assert: { type: "json" } });
+
+
+// add 4 radio buttons to switch between meanfield, stochastic, parallel, 3d, wellmixed data
+var form = d3.select("div#select-data")
+	.append("form")
+	.attr("id", "radio-buttons")
+	.attr("class", "radio-buttons");
+
+var form_label = form.append("label")
+	.attr("class", "radio-label")
+	.text("Meanfield (1)")
+	.append("input")
+	.attr("type", "radio")
+	.attr("name", "radio")
+	.attr("value", "meanfield")
+	.attr("checked", "checked")
+	.on("change", function() {
+		change_data(this.value)
+	});
+
+form_label.append("span")
+	.attr("class", "checkmark");
+
+form_label = form.append("label")
+	.attr("class", "radio-label")
+	.text("Stochastic (2)")
+	.append("input")
+	.attr("type", "radio")
+	.attr("name", "radio")
+	.attr("value", "stochastic")
+	.on("change", function() {
+		change_data(this.value)
+	});
+
+form_label.append("span")
+	.attr("class", "checkmark");
+
+form_label = form.append("label")
+	.attr("class", "radio-label")
+	.text("3D (3)")
+	.append("input")
+	.attr("type", "radio")
+	.attr("name", "radio")
+	.attr("value", "3D")
+	.on("change", function() {	
+		change_data(this.value)
+	});
+
+form_label.append("span")
+	.attr("class", "checkmark");
+
+form_label = form.append("label")
+	.attr("class", "radio-label")
+	.text("Well-mixed (4)")
+	.append("input")
+	.attr("type", "radio")
+	.attr("name", "radio")
+	.attr("value", "wellmixed")
+	.on("change", function() {	
+		change_data(this.value)
+	});
+
+
+// on 1,2,3,4,5 set radio buttons
+document.addEventListener('keydown', function(event) {
+	if (event.code === 'Digit1') {
+		// set radio button to meanfield
+		document.getElementById("radio-buttons").elements[0].checked = true;
+		change_data('meanfield')
+	}
+	else if (event.code === 'Digit2') {
+		// set radio button to stochastic
+		document.getElementById("radio-buttons").elements[1].checked = true;
+		change_data('stochastic')
+	}
+	else if (event.code === 'Digit3') {
+		// set radio button to 3D
+		document.getElementById("radio-buttons").elements[2].checked = true;
+		change_data('3D')
+	}
+	else if (event.code === 'Digit4') {
+		// set radio button to wellmixed
+		document.getElementById("radio-buttons").elements[3].checked = true;
+		change_data('wellmixed')
+	}
+});
+
+
+let step_list = data_meanfield.reduce(function (a, d) {
+    if (a.indexOf(d.step) === -1) {
+        a.push(d.step);
+	}
+	return a;
+}, []);
+const step_meanfield = d3.max(step_list);
+
+step_list = data_stochastic.reduce(function (a, d) {
+    if (a.indexOf(d.step) === -1) {
+      a.push(d.step);
+    }
+    return a;
+ }, []);
+const step_stochastic = d3.max(step_list);
+
+// step_list = data_3D.reduce(function (a, d) {
+// 	if (a.indexOf(d.step) === -1) {
+// 	  a.push(d.step);
+// 	}
+// 	return a;
+//  }, []);
+// const step_3D = d3.max(step_list);
+
+step_list = data_wellmixed.reduce(function (a, d) {
+	if (a.indexOf(d.step) === -1) {
+	  a.push(d.step);
+	}
+	return a;
+ }, []);
+const step_wellmixed = d3.max(step_list);
+
+
+data_meanfield = data_meanfield.filter(function(d) {return d.step == step_meanfield});
+data_stochastic = data_stochastic.filter(function(d) {return d.step == step_stochastic});
+// data_3D = data_3D.filter(function(d) {return d.step == step_3D});
+data_wellmixed = data_wellmixed.filter(function(d) {return d.step == step_wellmixed});
+
+console.log(data_meanfield);
+console.log(data_stochastic);
+// console.log(data_3D);
+console.log(data_wellmixed);
+
+let data = data_meanfield;
+
+data_stochastic.forEach((d) => {
+	const lattice = d.soil_lattice;
+	const L = lattice.length
+	// calculate the fraction of 1s, 2s and 0s in the matrix
+	const zeros = lattice.reduce((a, b) => a + b.filter((x) => x === 0).length, 0);
+	const ones = lattice.reduce((a, b) => a + b.filter((x) => x === 1).length, 0);
+	const twos = lattice.reduce((a, b) => a + b.filter((x) => x === 2).length, 0);
+	const threes = lattice.reduce((a, b) => a + b.filter((x) => x === 3).length, 0);
+	const fours = lattice.reduce((a, b) => a + b.filter((x) => x === 4).length, 0);
+	d.redvacancy = zeros / L**2;
+	d.bluevacancy = ones / L**2;
+	d.soil = twos / L**2;
+	d.red = threes / L**2;
+	d.blue = fours / L**2;
+});
+
+// data_3D.forEach((d) => {
+// 	const lattice = d.soil_lattice;
+// 	const L = lattice.length
+// 	// calculate the fraction of 1s, 2s and 0s in the 3D matrix
+// 	const ones = lattice.reduce((a, b) => a + b.reduce((c, d) => c + d.filter((x) => x === 1).length, 0), 0);
+// 	const twos = lattice.reduce((a, b) => a + b.reduce((c, d) => c + d.filter((x) => x === 2).length, 0), 0);
+// 	const zeros = lattice.reduce((a, b) => a + b.reduce((c, d) => c + d.filter((x) => x === 0).length, 0), 0);
+// 	d.vacancy = zeros / L**3;
+// 	d.soil = ones / L**3;
+// 	d.bacteria = twos / L**3;
+// });
+
+data_wellmixed.forEach((d) => {
+	const lattice = d.soil_lattice;
+	const L = lattice.length
+	// calculate the fraction of 1s, 2s and 0s in the matrix
+	const zeros = lattice.reduce((a, b) => a + b.filter((x) => x === 0).length, 0);
+	const ones = lattice.reduce((a, b) => a + b.filter((x) => x === 1).length, 0);
+	const twos = lattice.reduce((a, b) => a + b.filter((x) => x === 2).length, 0);
+	const threes = lattice.reduce((a, b) => a + b.filter((x) => x === 3).length, 0);
+	const fours = lattice.reduce((a, b) => a + b.filter((x) => x === 4).length, 0);
+	d.redvacancy = zeros / L**2;
+	d.bluevacancy = ones / L**2;
+	d.soil = twos / L**2;
+	d.red = threes / L**2;
+	d.blue = fours / L**2;
+});
+
+
+// set the dimensions and margins of the graph
+var margin = {top: 40, right: 40, bottom: 100, left: 100},
+  width = innerWidth*0.8 - margin.left - margin.right,
+  height = innerHeight - margin.top - margin.bottom;
+
+
+function getOffset(element) {
+  const rect = element.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  };
+}
+
+var rows = d3.map(data_meanfield, function(d){return d.d;})
+var cols = d3.map(data_meanfield, function(d){return d.s;}) 
+
+var x = d3.scaleBand()
+	.range([0, width])
+	.domain(rows)
+	.padding(0.05);
+
+var y = d3.scaleBand()
+	.range([height, 0])
+	.domain(cols)
+	.padding(0.05);
+          
+// append the svg object to the body of the page
+var svg_soil = d3.select("div#raster")
+  .append("svg")
+    .attr("id", "soil_amounts")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+var Tooltip = d3.select("div#raster")
+	.append("div")
+	.style("opacity", 0)
+	.attr("class", "tooltip") 
+	// Three function that change the tooltip when user hover / move / leave a cell
+	var mouseover_rgb = function(event, d) {
+	Tooltip
+		.style("opacity", 1)
+		.style("position", "absolute")
+	d3.select(this)
+		.style("stroke", "black")
+	}
+	var mousemove_rgb = function(event, d) {
+	var heatmap2_location = getOffset(document.getElementById("soil_amounts"))
+	Tooltip
+        .html("d=" + d3.format("0.2f")(d.d) + ", " + "s=" + d3.format("0.2f")(d.s) + "<br>" + d3.format("0.2f")(d.soil) + ", " + d3.format("0.2f")(d.redvacancy + d.bluevacancy) + ", " + d3.format("0.2f")(d.red + d.blue))
+		.style("left", (d3.pointer(event)[0] + heatmap2_location.left + 30) + "px")
+		.style("top", (d3.pointer(event)[1] + heatmap2_location.top - 20) + "px")
+	}
+	var mouseleave_rgb = function(event, d) {
+	Tooltip
+		.style("opacity", 0)
+	d3.select(this)
+		.style("stroke", "none")
+	}
+
+svg_soil.append("g")
+	.attr("class", "axis")
+	.attr("transform", "translate(0," + height + ")")
+	.call(d3.axisBottom(x).tickFormat(d3.format(".2f")))
+	.selectAll("text")
+		.attr("transform", "translate(-10,0)rotate(-45)")
+		.style("text-anchor", "end");
+
+// label axes
+svg_soil.append("text")
+	.attr("class", "axis_label")
+	.attr("transform", "translate(" + (width/2) + " ," + (height + margin.top) + ")")
+	.style("text-anchor", "middle")
+	.text("d");
+
+
+svg_soil.append("g")
+	.attr("class", "axis")
+	.call(d3.axisLeft(y).tickFormat(d3.format(".2f")));
+
+// label y axis
+svg_soil.append("text")
+.attr("class", "axis_label")
+.attr("transform", "rotate(-90)")
+.attr("y", 0 - margin.left/1.5)
+.attr("x",0 - (height / 2))
+.attr("dy", "1em")
+.style("text-anchor", "middle")
+.text("s");
+
+
+svg_soil.selectAll(".cell")
+	.data(data)
+	.enter()
+	.append("rect")
+		.attr("class", "cell")
+		.attr("x", function(d) { return x(d.d) })
+		.attr("y", function(d) { return y(d.s) })
+		.attr("width", x.bandwidth() )
+		.attr("height", y.bandwidth() )
+		.style("fill", function(d) { return "rgb(" + d.soil*255 + "," + (d.redvacancy+d.bluevacancy)*255 + "," + (d.red+d.blue)*255 + ")" } )
+		.on("mouseover", mouseover_rgb)
+		.on("mousemove", mousemove_rgb)
+		.on("mouseleave", mouseleave_rgb);
+
+
+function change_data(state) {
+	
+	const t = d3.transition().duration(750)
+
+	console.log('Changing data');
+
+	if (state == 'meanfield') {
+		data = data_meanfield;
+	} 
+	else if (state == 'stochastic') {
+		data = data_stochastic;
+	} 
+	else if (state == '3D') {
+		data = data_3D;
+	}
+	else if (state == 'wellmixed') {
+		data = data_wellmixed;
+	}
+
+	// update the rgb heatmap
+	svg_soil.selectAll(".cell")
+		.data(data)
+		.transition(t)
+		.style("fill", function(d) { return "rgb(" + d.soil*255 + "," + (d.redvacancy+d.bluevacancy)*255 + "," + (d.red+d.blue)*255 + ")" } );
+
+}
+	
---
+++ src/two_species_rw/simulation_raster.py
@@ -0,0 +1,96 @@
+import numpy as np
+from tqdm import tqdm
+import pandas as pd
+from two_species_utils import run_stochastic, run_stochastic_wellmixed
+
+
+def run_raster_stochastic(n_steps, L, r, d_list, s_list, steps_to_record=np.array([100, 1000, 10000, 100000])):
+    """Run the rasterscan for the stochastic case for n_steps timesteps.
+    
+    Parameters
+    ----------
+    n_steps : int
+        Number of timesteps to run the simulation for.
+    L : int
+        Side length of the square lattice.
+    r : float
+        Reproduction rate.
+    d_list : ndarray
+        List of death rates.
+    s_list : ndarray
+        List of soil filling rates.
+    steps_to_record : ndarray, optional
+        Array of timesteps to record the lattice data for, by default [100, 1000, 10000, 100000].
+    
+    Returns
+    -------
+    soil_lattice_list : list
+        List of soil_lattice data for specific timesteps and parameters.
+    """
+    grid = np.meshgrid(d_list, s_list)
+    ds_pairs = np.reshape(grid, (2, -1)).T  # all possible pairs of d and s
+    soil_lattice_list = []
+    for i in tqdm(range(len(ds_pairs))):  # todo: parallelize
+        d, s = ds_pairs[i]
+        soil_lattice_data = run_stochastic(n_steps, L, r, d, s, steps_to_record=steps_to_record)
+        for step in steps_to_record:
+            soil_lattice_list.append({"d": d, "s": s, "step":step, "soil_lattice": soil_lattice_data[steps_to_record == step][0]})
+    return soil_lattice_list
+
+
+def run_raster_stochastic_wellmixed(n_steps, L, r, d_list, s_list, steps_to_record=np.array([100, 1000, 10000, 100000])):
+    """Run the rasterscan for the well-mixed stochastic case for n_steps timesteps.
+    
+    Parameters
+    ----------
+    n_steps : int
+        Number of timesteps to run the simulation for.
+    L : int
+        Side length of the square lattice.
+    r : float
+        Reproduction rate.
+    d_list : ndarray
+        List of death rates.
+    s_list : ndarray
+        List of soil filling rates.
+    steps_to_record : ndarray, optional
+        Array of timesteps to record the lattice data for, by default [100, 1000, 10000, 100000].
+    
+    Returns
+    -------
+    soil_lattice_list : list
+        List of soil_lattice data for specific timesteps and parameters.
+    """
+    grid = np.meshgrid(d_list, s_list)
+    ds_pairs = np.reshape(grid, (2, -1)).T  # all possible pairs of d and s
+    soil_lattice_list = []
+    for i in tqdm(range(len(ds_pairs))):  # todo: parallelize
+        d, s = ds_pairs[i]
+        soil_lattice_data = run_stochastic_wellmixed(n_steps, L, r, d, s, steps_to_record=steps_to_record)
+        for step in steps_to_record:
+            soil_lattice_list.append({"d": d, "s": s, "step":step, "soil_lattice": soil_lattice_data[steps_to_record == step][0]})
+    return soil_lattice_list
+
+
+def main():
+
+    # initialize the parameters
+    n_steps = 10_000_000  # number of bacteria moves
+    L = 50  # side length of the square lattice
+    r = 1  # reproduction rate
+    d = np.linspace(0, 0.1, 20)  # death rate (same as mean field)
+    s = np.linspace(0, 0.8, 20)  # soil filling rate (same as mean field)
+
+    # # NEIGHBOURS
+    # soil_lattice_data = run_raster_stochastic(n_steps, L, r, d, s, np.geomspace(100, n_steps, int(np.log10(n_steps/100))+1, dtype=np.int32))
+    # soil_lattice_data = pd.DataFrame(soil_lattice_data)
+    # soil_lattice_data.to_json(f"docs/data/two_species/stochastic_dynamics_{r=}.json", orient="records")
+
+    # WELLMIXED
+    soil_lattice_data = run_raster_stochastic_wellmixed(n_steps, L, r, d, s, np.geomspace(100, n_steps, int(np.log10(n_steps/100))+1, dtype=np.int32))
+    soil_lattice_data = pd.DataFrame(soil_lattice_data)
+    soil_lattice_data.to_json(f"docs/data/two_species/wellmixed_stochastic_dynamics_{r=}.json", orient="records")
+    
+
+if __name__ == "__main__":
+    main()
