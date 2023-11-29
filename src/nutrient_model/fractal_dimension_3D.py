2e2e553039085e40df1baf78b31c71f07bffe235
Riz <87264061+rizfn@users.noreply.github.com>
2023-11-29 17:21:25 +0100

    Add bad data
    I was sick

--- src/nutrient_model/fractal_dimension_3D.py
+++ src/nutrient_model/fractal_dimension_3D.py
@@ -202,10 +202,10 @@ def main():
     n_steps = steps_per_latticepoint * L**3  # number of bacteria moves
     rho = 1  # reproduction rate
     theta = 0.04  # death rate
-    sigma = 0.68  # soil filling rate
+    sigma = 0.5  # soil filling rate
     delta = 0 # nutrient decay rate
 
-    steps_to_record = np.arange(n_steps//2, n_steps, 20 * L**2, dtype=np.int32)
+    steps_to_record = np.arange(n_steps//2, n_steps, 20 * L**3, dtype=np.int32)
 
     # run the simulation
     soil_lattice_data = run_stochastic_3D(n_steps, L, rho, theta, sigma, delta, steps_to_record)
@@ -213,7 +213,60 @@ def main():
     # calculate the cluster sizes
     soil_cluster_sizes = np.concatenate(calculate_cluster_sizes(soil_lattice_data, 2))
 
-     # Calculate the lengths and volumes of the clusters
+    # plot cluster size distribution with power law of slope tau
+    plt.figure(figsize=(12, 8))
+    num_bins = 100
+    min_size = 1  # smallest cluster size
+    max_size = max(soil_cluster_sizes)
+    bins = np.logspace(np.log10(min_size), np.log10(max_size), num=num_bins)
+    # Calculate histograms and plot
+    hist, edges = np.histogram(soil_cluster_sizes, bins=bins, density=False)
+    bin_widths = np.diff(edges)
+    hist = hist / bin_widths  # Normalize by bin width
+    plt.plot(edges[:-1], hist, 'x', label='Soil Sites')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('Cluster Size')
+    plt.ylabel('Probability Density')
+
+    # # plot power law with exponent gamma
+    # gamma = 2.3
+    # x = np.array(edges[:-1])
+    # plt.plot(x, 2e6*x**-gamma, label=r'$\gamma$ = ' + f'{gamma} power law', linestyle='--')
+    # plt.title(f'{L=}, {rho=}, {theta=}, {sigma=}, {delta=}')
+    # plt.legend()
+    # plt.savefig(f'src/nutrient_model/plots/cluster_sizes_3D_{theta=}_{sigma=}.png', dpi=300)
+    # plt.show()
+
+    # Convert to numpy arrays and remove zeros to avoid log(0)
+    x = np.array(edges[:-1])
+    y = np.array(hist)
+    mask = (y > 0) & (x > 0)
+    x = x[mask]
+    y = y[mask]
+
+    # Calculate the logarithm of x and y
+    log_x = np.log10(x)
+    log_y = np.log10(y)
+
+    # Remove or replace NaN and infinite values
+    log_x = np.nan_to_num(log_x, nan=0.0, posinf=0.0, neginf=0.0)
+    log_y = np.nan_to_num(log_y, nan=0.0, posinf=0.0, neginf=0.0)
+
+    # Use polyfit to fit a 1-degree polynomial to the log-log data
+    coeffs = np.polyfit(log_x, log_y, 1)
+    # The slope of the line is -gamma
+    gamma = -coeffs[0]
+
+    # Plot the power law with the calculated exponent gamma
+    plt.plot(x, 10**coeffs[1] * x**-gamma, label=r'$\gamma$ = ' + f'{gamma:.2f} power law', linestyle='--')
+    plt.title(f'{L=}, {rho=}, {theta=}, {sigma=}, {delta=}')
+    plt.legend()
+    plt.savefig(f'src/nutrient_model/plots/cluster_sizes_3D_{theta=}_{sigma=}.png', dpi=300)
+    plt.show()
+
+
+    # Calculate the lengths and volumes of the clusters
     lengths = (soil_cluster_sizes)**(1/3)
     volumes = soil_cluster_sizes
 
@@ -239,18 +292,21 @@ def main():
     plt.title(f'{L=}, {rho=}, {theta=}, {sigma=}, {delta=}')
 
     # Plot the log of the maximum normalized cumulative volumes against the log of the normalized lengths
-    plt.plot(np.log10(grouped.index), np.log10(grouped['Normalized Cumulative Volumes']), marker='x', linestyle='none', label='Simulation')
+    plt.plot(np.log10(grouped.index), np.log10(grouped['Normalized Cumulative Volumes']), marker='.', linestyle='none', label='Simulation')
     plt.xlabel('Log of Normalized Length')
     plt.ylabel('Log of Normalized Cumulative Volumes')
 
     # plot power-law with exponent tau
-    tau = 0.5
-    plt.plot(np.log10(normalized_lengths), tau * np.log10(normalized_lengths) , label=f'Slope = {tau}', linestyle='--')
+    tau = (4 - gamma) / 3    
+    # Find the index of the data point closest to x=-0.8
+    idx = np.abs(np.log10(normalized_lengths) + 0.8).argmin()
+    # Calculate the y-value of the line at x=-0.8
+    y_line = tau * -0.8
+    # Calculate the y-shift
+    y_shift = np.log10(normalized_cumulative_volumes)[idx] - y_line
 
-    # plot power-law with exponent tau
-    tau = 0.4
-    plt.plot(np.log10(normalized_lengths), tau * np.log10(normalized_lengths) - 0.1 , label=f'Slope = {tau}', linestyle='--')
-
+    # Plot the line with the calculated y-shift
+    plt.plot(np.log10(normalized_lengths), tau * np.log10(normalized_lengths) + y_shift, label=f'Slope = {tau:.2f}', linestyle='--')
     plt.legend()
     plt.savefig(f'src/nutrient_model/plots/fractal_dimension_3D_{theta=}_{sigma=}.png', dpi=300)
     plt.show()
