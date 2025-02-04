e770c39b267ea460efaf191c54d391532c2bc284
Riz <87264061+rizfn@users.noreply.github.com>
2025-02-04 19:48:15 +0900

    Update `viz_soilboundaries` for paper

--- src/nutrient_model/viz_soil_boundaries.py
+++ src/nutrient_model/viz_soil_boundaries.py
@@ -55,5 +55,69 @@ def main():
     plt.savefig("src/nutrient_model/plots/soil_boundaries/2D.png")
     plt.show()
 
+
+def plot_paper():
+    # Define the colors for the different sigma values
+    colors = plt.cm.Reds([0.5, 0.7, 1])
+    plt.rcParams['axes.prop_cycle'] = plt.cycler(color=colors)
+    equilibrium_step_fraction = 3/4  # Fraction of the final steps to consider for the equilibrium
+    data_list_2D = []
+
+    for filename in glob("src/nutrient_model/outputs/soil_boundaries/*.tsv"):
+        sub_df = pd.read_csv(filename, sep='\t')
+        sigma = filename.split("_")[3]
+        theta = filename.split("_")[5].rsplit(".", 1)[0]
+                
+        sub_df["sigma"] = sigma
+        sub_df["theta"] = theta
+        data_list_2D.append(sub_df)
+    
+    df = pd.concat(data_list_2D)
+
+    quantities = ["soil_nonsoil_boundaries", "nutrient_production"]
+    num_quantities = len(quantities)
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+    axs = axs.flatten()
+
+    sigma_values = df["sigma"].unique()
+    sigma_values = [sigma for sigma in sigma_values if sigma != "0"]
+
+    for sigma, color in zip(sigma_values, colors):
+        same_sigma = df[df["sigma"] == sigma]
+        theta_values = []
+
+        mean_values = {quantity: [] for quantity in quantities}
+
+        for theta in same_sigma["theta"].unique():
+            if theta == "0":
+                continue
+            same_theta = same_sigma[same_sigma["theta"] == theta]
+            final_fraction = same_theta.iloc[int(len(same_theta) * (1 - equilibrium_step_fraction)):]  # Select the final fraction steps
+            theta_values.append(float(theta))
+
+            for quantity in quantities:
+                mean_value = final_fraction[quantity].mean()  # Calculate the mean value for the quantity
+                mean_values[quantity].append(mean_value)
+
+        for i, quantity in enumerate(quantities):
+            axs[i].plot(theta_values, mean_values[quantity], marker='o', label=f'sigma = {sigma}', color=color)
+
+    y_labels = ["Soil boundary fraction", "Nutrient production"]
+    for i, quantity in enumerate(quantities):
+        axs[i].set_xlabel("Theta")
+        axs[i].set_ylabel(y_labels[i])
+        axs[i].grid()
+        axs[i].legend()
+
+        # Add vertical dashed lines
+        axs[i].axvline(x=0.13, color=colors[0], linestyle='--', label='sigma=0.1 soil power law', alpha=0.8)
+        axs[i].axvline(x=0.14, color=colors[1], linestyle='--', label='sigma=0.5 soil power law', alpha=0.8)
+        axs[i].axvline(x=0.134, color=colors[2], linestyle='--', label='sigma=1.0 soil power law', alpha=0.8)
+
+    plt.tight_layout()
+    plt.savefig('src/nutrient_model/plots/soil_boundaries/2quantities_soilboundaries_2D.png')
+    plt.show()
+
 if __name__ == "__main__":
-    main()
+    # main()
+    plot_paper()
--- src/nutrient_model_two_species/viz_soilboundaries.py
+++ src/nutrient_model_two_species/viz_soilboundaries.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from glob import glob
 
 def main():
+    L = 2048
 
     data_list = []
 
@@ -20,7 +21,7 @@ def main():
 
     df['soil_nutrient_boundaries'] = df['soil_greennutrient_boundaries'] + df['soil_bluenutrient_boundaries']
     df['soil_worm_boundaries'] = df['soil_greenworm_boundaries'] + df['soil_blueworm_boundaries']
-    df['soil_empty_boundaries']  =df['soil_nonsoil_boundaries'] - df['soil_nutrient_boundaries'] - df['soil_worm_boundaries']
+    df['soil_empty_boundaries'] = df['soil_nonsoil_boundaries'] - df['soil_nutrient_boundaries'] - df['soil_worm_boundaries']
     df['nutrients'] = df['green_nutrients'] + df['blue_nutrients']
     df['worms'] = df['green_worms'] + df['blue_worms']
     df['nutrient_production'] = df['green_nutrient_production'] + df['blue_nutrient_production']
@@ -63,8 +64,76 @@ def main():
             axs[row, col].legend()
 
     plt.tight_layout()
-    plt.savefig('src/nutrient_model_two_species/plots/soil_boundaries/all_themes_L_2048.png', dpi=300)
+    plt.savefig(f'src/nutrient_model_two_species/plots/soil_boundaries/all_themes_L_{L}.png', dpi=300)
     plt.show()
+
+
+def plot_paper():
+    L = 2048
+
+    data_list = []
+
+    for filename in glob(f"src/nutrient_model_two_species/outputs/sametheta/soil_boundaries/*.tsv"):
+        sub_df = pd.read_csv(filename, sep='\t')
+        sigma = float(filename.split("_")[5])
+        theta = float(filename.split("_")[7].rsplit(".", 1)[0])
+                
+        sub_df["sigma"] = sigma
+        sub_df["theta"] = theta
+
+        data_list.append(sub_df)
     
+    df = pd.concat(data_list)
+
+    df['soil_nonsoil_boundaries'] /= (2 * L * L)
+    df['nutrient_production'] = df['green_nutrient_production'] + df['blue_nutrient_production']
+
+    quantities = ["soil_nonsoil_boundaries", "nutrient_production"]
+    colors = plt.cm.Reds([0.5, 0.7, 1])
+    plt.rcParams['axes.prop_cycle'] = plt.cycler(color=colors)
+    plt.rcParams['font.size'] = 16
+
+    fig, axs = plt.subplots(2, 1, figsize=(6.5, 12), sharex=True)
+    axs = axs.flatten()
+
+    sigma_values = df["sigma"].unique()
+    sigma_values = [sigma for sigma in sigma_values if sigma != 0]
+
+    for sigma, color in zip(sigma_values, colors):
+        same_sigma = df[df["sigma"] == sigma]
+        theta_values = sorted(same_sigma["theta"].unique())
+
+        mean_values = {quantity: [] for quantity in quantities}
+
+        for theta in theta_values:
+            same_theta = same_sigma[same_sigma["theta"] == theta]
+            mean_theta_values = same_theta[quantities].mean()
+
+            for quantity in quantities:
+                mean_values[quantity].append(mean_theta_values[quantity])
+
+        for i, quantity in enumerate(quantities):
+            axs[i].plot(theta_values, mean_values[quantity], marker='.', label=f'$\sigma$ = {sigma}', color=color)
+
+    y_labels = ["Soil boundaries", "Nutrient production"]
+    for i, quantity in enumerate(quantities):
+        axs[i].set_ylabel(y_labels[i], fontsize=20)
+        axs[i].grid()
+        if i == 0:
+            axs[i].legend()
+
+        # Add vertical dashed lines
+        critical_points = {0.3: 0.0315, 0.6: 0.039, 1.0: 0.04125}
+        axs[i].axvline(x=critical_points[0.3], color=colors[0], linestyle='--', label='$\sigma$=0.3 soil power law', alpha=0.8)
+        axs[i].axvline(x=critical_points[0.6], color=colors[1], linestyle='--', label='$\sigma$=0.6 soil power law', alpha=0.8)
+        axs[i].axvline(x=critical_points[1.0], color=colors[2], linestyle='--', label='$\sigma$=1.0 soil power law', alpha=0.8)
+
+    axs[-1].set_xlabel("Worm death rate ($\\theta$)", fontsize=20)
+
+    plt.tight_layout()
+    plt.savefig('src/nutrient_model_two_species/plots/soil_boundaries/2quantities_soilboundaries_2D.png', dpi=300)
+    plt.show()
+
 if __name__ == "__main__":
-    main()
+    # main()
+    plot_paper()
